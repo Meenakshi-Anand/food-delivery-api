35e6eb6d9d402e308dcb594eb9d891be1829fe55
Meenakshi <abhi@codeanand.com>
2020-05-12 23:27:45 -0700

    update config.ru with rake cors information

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,6 @@ module FoodDeliveryRailsApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join('lib')
-    config.middleware.insert_before ActionDispatch::Static, Rails::Cors do
-     allow do
-       origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
-     end
    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
